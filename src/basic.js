9179f90a592c213c8ebd2bc769bd2ee43e841446
MabelZar <mabel.zar.esp@gmail.com>
2024-06-10 00:04:31 +0200

    try1

--- src/basic.js
+++ src/basic.js
@@ -1,13 +1,59 @@
 function welcome(){
     //Escribe tu código aquí
+
+   const saludo="Hello World";
+   return saludo;
 };
 
-function evenOrOdd(n){
-    //Escribe tu código aquí
-};
+function evenOrOdd(numero) {
+    if (numero%2===0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
 
-function greeting(name) {
-    //Escribe tu código aquí
-};
+
+function greeting(persona) {
+    return `Hola, ${persona}!`;
+}
 
 export { evenOrOdd, greeting, welcome }
+
+//ejercicio 2
+function compOne() {
+    const number= 20;
+    const comNum=(number===20);
+    
+return comNum;
+}
+
+function compTwo() {
+    const language= "Javascript";
+    const expresion=(language!== "Javascript");
+    return expresion;
+}
+export{ compOne, compTwo}
+
+//ejercicio 3
+function operaciones() {
+    const a=50;
+    const b=50;
+    const suma=a+b;
+    const multiplicacion=a*b;
+    const divicion=a/b;
+    return [suma , multiplicacion , divicion];
+}
+export{operaciones}
+
+//ejercicio 4
+function and(a,b) {
+    return a && b;
+}
+function oo(a,b) {
+    return a || b;
+    
+}
+export{and, oo}
+
+
